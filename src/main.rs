c03993f849a9e42605bc63ff5f7ca3bce1e1d054
Sakurai <K.Sakurai00@outlook.com>
2024-10-22 14:27:07 +0900

    feat: hostgrep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+use clap::Parser;
+use anyhow::Result;
+
+#[derive(Debug,Parser)]
+struct Args {
+    #[arg(help = "検索ワード")]
+    search_word: String,
+}
+
+fn main() -> Result<()> {
+    let path: &str = "/etc/hosts";
+    let args = Args::parse();
+
+    hostgrep(path, args.search_word)?;
+
+    Ok(())
+}
+
+fn hostgrep(path: &str, word: String) -> Result<()> {
+    let file = File::open(path)?;
+    let buf = BufReader::new(file);
+
+    for line in buf.lines() {
+        let line = line?;
+        if line.contains(&word) {
+            println!("{}", line);
+        }
+    }
+    Ok(())
 }
